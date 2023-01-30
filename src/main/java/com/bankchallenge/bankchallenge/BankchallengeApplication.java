30e8d84aaad64cf4033993038341f8f273e9de59
mariasolomon <30388010+mariasolomon@users.noreply.github.com>
2023-01-30 16:33:51 +0100

    initiation of Spring Boot project with maven as build tool
    initiation of Spring Boot project with maven as build tool

---
+++ src/main/java/com/bankchallenge/bankchallenge/BankchallengeApplication.java
@@ -0,0 +1,12 @@
+package com.bankchallenge.bankchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankchallengeApplication.class, args);
+	}
+}
